3092c8f7091050a438756a9f7c032480074008d7
AkramExp <akram.exp02@gmail.com>
2023-12-14 22:30:12 +0530

    added cabin components

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import CabinTable from "../features/cabins/CabinTable";
+import Row from "../ui/Row";
+import Heading from "../ui/Heading";
 
 export default function Cabins() {
-  return <div>Cabins</div>;
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All Cabins</Heading>
+        <p>sort/filter</p>
+      </Row>
+      <Row type="vertical">
+        <CabinTable />
+      </Row>
+    </>
+  );
 }
