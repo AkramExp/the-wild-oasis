180de3304213d3ce94b89980da1f669880499461
AkramExp <akram.exp02@gmail.com>
2023-12-15 17:37:56 +0530

    minor changes

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://apbuylfswlgwckhbodid.supabase.co";
+export const supabaseUrl = "https://apbuylfswlgwckhbodid.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwYnV5bGZzd2xnd2NraGJvZGlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI0OTI5MTcsImV4cCI6MjAxODA2ODkxN30.t8bUFOvyvpR1MR3ueythYZKAkFDs3E2UtPqa-iVvPEw";
 const supabase = createClient(supabaseUrl, supabaseKey);
