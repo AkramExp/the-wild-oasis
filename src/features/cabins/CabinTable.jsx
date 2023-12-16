f56cd50cafd6c17437141b60925c863276c30575
AkramExp <akram.exp02@gmail.com>
2023-12-16 20:35:35 +0530

    added a context menu in modal

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,30 +1,31 @@
-import styled from "styled-components";
+import Menus from "../../ui/Menus";
 import Spinner from "../../ui/Spinner";
+import Table from "../../ui/Table";
 import CabinRow from "./CabinRow";
 import useCabins from "./useCabins";
 
-const Table = styled.div`
-  background-color: var(--color-grey-0);
-  border: 1px solid var(--color-grey-200);
-  font-size: 1.4rem;
-  border-radius: 7px;
-  overflow: hidden;
-`;
+// const Table = styled.div`
+//   background-color: var(--color-grey-0);
+//   border: 1px solid var(--color-grey-200);
+//   font-size: 1.4rem;
+//   border-radius: 7px;
+//   overflow: hidden;
+// `;
 
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr repeat(3, 1fr);
-  column-gap: 2.4rem;
-  align-items: center;
+// const TableHeader = styled.header`
+//   display: grid;
+//   grid-template-columns: 0.6fr 1.8fr 2.2fr repeat(3, 1fr);
+//   column-gap: 2.4rem;
+//   align-items: center;
 
-  background-color: var(--color-grey-50);
-  color: var(--color-grey-600);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  padding: 1.6rem 2.4rem;
-`;
+//   background-color: var(--color-grey-50);
+//   color: var(--color-grey-600);
+//   border-bottom: 1px solid var(--color-grey-100);
+//   text-transform: uppercase;
+//   letter-spacing: 0.4px;
+//   font-weight: 600;
+//   padding: 1.6rem 2.4rem;
+// `;
 
 export default function CabinTable() {
   const { isLoading, cabins } = useCabins();
@@ -32,18 +33,21 @@ export default function CabinTable() {
   if (isLoading) return <Spinner />;
 
   return (
-    <Table>
-      <TableHeader>
-        <div></div>
-        <div>Cabin</div>
-        <div>Capacity</div>
-        <div>Price</div>
-        <div>Discount</div>
-        <div></div>
-      </TableHeader>
-      {cabins.map((cabin) => (
-        <CabinRow cabin={cabin} key={cabin.id} />
-      ))}
-    </Table>
+    <Menus>
+      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 0.5fr">
+        <Table.Header>
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
+        <Table.Body
+          data={cabins}
+          render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+        />
+      </Table>
+    </Menus>
   );
 }
