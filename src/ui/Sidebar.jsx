eec5048ef889800d762a3d40b436f1ece3351626
AkramExp <akram.exp02@gmail.com>
2023-12-17 19:22:16 +0530

    minor changes

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
 
 const StyledSidebar = styled.aside`
   padding: 3.2rem 2.4rem;
   background-color: var(--color-grey-0);
   grid-row: 1/-1;
   border-right: var(--color-grey-100);
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
+
 export default function Sidebar() {
   return (
     <StyledSidebar>
       <Logo />
       <MainNav />
+
+      <Uploader />
     </StyledSidebar>
   );
 }
