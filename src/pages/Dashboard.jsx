721868f14f49638e2b21f770aac34ba05cb425c5
AkramExp <akram.exp02@gmail.com>
2023-12-24 20:35:34 +0530

    added stats on dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import Row from "../ui/Row";
+import Heading from "../ui/Heading";
+import DashboardFilter from "../features/dashboard/DashboardFilter";
+import DashboardLayout from "../features/dashboard/DashboardLayout";
 
 export default function dashboard() {
-  return <div>dashboard</div>;
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <DashboardFilter />
+      </Row>
+      <DashboardLayout />
+    </>
+  );
 }
