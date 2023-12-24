5c4089573c410569a1186ce80b7579f1bcc3711c
AkramExp <akram.exp02@gmail.com>
2023-12-24 12:36:24 +0530

    created header menu and avatar section

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import Logout from "../features/authentication/Logout";
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/users/UserAvatar";
 
 const StyledHeader = styled.header`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  justify-content: flex-end;
   padding: 1.2rem 4.8rem;
   background-color: var(--color-grey-0);
   border-bottom: 1px solid var(--color-grey-100);
@@ -10,7 +15,8 @@ const StyledHeader = styled.header`
 export default function Header() {
   return (
     <StyledHeader>
-      <Logout />
+      <UserAvatar />
+      <HeaderMenu />
     </StyledHeader>
   );
 }
